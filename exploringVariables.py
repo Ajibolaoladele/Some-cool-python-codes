4d53814ae2c82f673d7cd76292bc79f057a831a7
Brad Penney <penney.brad@gmail.com>
2018-12-08 16:56:07 -0400

    added a couple tutorials

---
+++ exploringVariables.py
@@ -0,0 +1,25 @@
+#
+# text = "This is my message."
+#
+# print(text)
+#
+# text = "Lets change the message"
+#
+# print(text)
+#
+# print(len(text))
+#
+# print(text.lower())
+#
+# print(text.upper())
+
+# print (5*3)
+# print (5**3)
+
+
+
+bigNumber = 42
+littleNumber = 3
+message = "Forty-two multiplied by three equals: "
+
+print (message + str(bigNumber * littleNumber))
---
+++ helloWorld.py
@@ -0,0 +1,3 @@
+#Obligatory First Program
+
+print("Hello World!")
